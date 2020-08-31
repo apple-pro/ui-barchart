43ce774faf63ca1bbfbf7d0728414a49759da675
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-31 08:47:31 +0800

    Initial Commit

---
+++ ui-barchart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ui-barchart
+//
+//  Created by StartupBuilder.INFO on 8/31/20.
+//  Copyright © 2020 StartupBuilder.INFO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
